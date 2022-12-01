a0dc5cc13fa9193cf3e9802bfc18360d89eab788
kerwindows <kerwindows@hotmail.com>
2022-12-01 15:42:32 -0500

    Updates

---
+++ precache-manifest.a1bc0e7c380f0d9f853808200866640b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab9aa54b48c01d0f3cb",
+    "url": "/se_project_react/static/css/main.4d571d21.chunk.css"
+  },
+  {
+    "revision": "9ab9aa54b48c01d0f3cb",
+    "url": "/se_project_react/static/js/main.9ab9aa54.chunk.js"
+  },
+  {
+    "revision": "e8a858b4d510172b3b18",
+    "url": "/se_project_react/static/js/1.e8a858b4.chunk.js"
+  },
+  {
+    "revision": "48d9f85ffd2176d2504c",
+    "url": "/se_project_react/static/js/2.48d9f85f.chunk.js"
+  },
+  {
+    "revision": "de0331340d2561b5c339",
+    "url": "/se_project_react/static/js/runtime~main.de033134.js"
+  },
+  {
+    "revision": "9293aa8fd18af383d6eb4eeeecab483f",
+    "url": "/se_project_react/static/media/close-icon.9293aa8f.svg"
+  },
+  {
+    "revision": "dd31dd063f1c376deb28b582c4a64833",
+    "url": "/se_project_react/static/media/heart.dd31dd06.svg"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/se_project_react/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/se_project_react/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/se_project_react/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "ef38b085db5a81756d4dc5a64c1fccb8",
+    "url": "/se_project_react/static/media/heart-fill.ef38b085.svg"
+  },
+  {
+    "revision": "e49d1df3fb425d81ffa089ab4e39a191",
+    "url": "/se_project_react/static/media/logo.e49d1df3.svg"
+  },
+  {
+    "revision": "c1a2fbc10709581c0d7cb2de1418574a",
+    "url": "/se_project_react/static/media/hamburger-menu-svg-2.c1a2fbc1.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/se_project_react/static/media/successful.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/se_project_react/static/media/unsuccessful.4072126c.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/se_project_react/static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/se_project_react/static/media/Vector.d476a8b8.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/se_project_react/static/media/plus-sign.b2fce4ac.svg"
+  },
+  {
+    "revision": "7ee866f113eb2ed4216d588f5b956e6e",
+    "url": "/se_project_react/static/media/Trash.7ee866f1.svg"
+  },
+  {
+    "revision": "141198e07d41f343903b8a60f9d6d169",
+    "url": "/se_project_react/static/media/loader.141198e0.png"
+  },
+  {
+    "revision": "f833a5fd281ba1398374b5a98fe1b7b1",
+    "url": "/se_project_react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/around-react/precache-manifest.4828d63fa5a61c78b184668926321703.js"
+  "/se_project_react/precache-manifest.a1bc0e7c380f0d9f853808200866640b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/around-react/index.html", {
+workbox.routing.registerNavigationRoute("/se_project_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
