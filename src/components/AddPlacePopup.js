ecb7220d722c5c2bdac2cf3e8b15f4e9c1df9e49
kerwindows <kerwindows@hotmail.com>
2022-12-03 07:10:41 -0500

    added validation to add place popup form

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,35 +1,22 @@
 /* --------------------------------- imports -------------------------------- */
 import React from "react";
 import PopupWithForm from "./PopupWithForm";
+import { useForm } from "react-hook-form";
 
 /* ------------------------ function EditProfilePopup ----------------------- */
 
 function AddPlacePopup(props) {
-  const [name, setName] = React.useState("");
-  const [link, setLink] = React.useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  React.useEffect(() => {
-    setName("");
-    setLink("");
-  }, [props.isOpen]);
-
-  function handleNameChange(evt) {
-    setName(evt.target.value);
-  }
-
-  function handleLinkChange(evt) {
-    setLink(evt.target.value);
-  }
-
-  function handleSubmit(e) {
-    // Prevent the browser from navigating to the form address
-    e.preventDefault();
-
-    // Pass the values of the managed components to the external handler
-    props.onAddPlaceSubmit({
-      name,
-      link,
-    });
+  function handleSubmitPost(validatedData, e) {
+    props.onAddPlaceSubmit(validatedData);
+    e.target.reset();
+    reset({});
   }
 
   return (
@@ -39,32 +26,44 @@ function AddPlacePopup(props) {
       submitText={props.isLoading ? "Saving..." : "Create"}
       isOpen={props.isOpen}
       onClose={props.onClose}
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(handleSubmitPost)}
     >
       <input
         id='js-input-place-name-input'
         className='popup__form-input js-input-type-place-name'
-        name='name'
         type='text'
-        minLength='2'
-        maxLength='30'
         placeholder='Title'
-        onChange={handleNameChange}
-        value={name}
-        required
+        {...register("name", {
+          required: "Image name is required",
+          minLength: {
+            value: 2,
+            message: "Name must be at least 2 characters",
+          },
+          maxLength: {
+            value: 30,
+            message: "Name cannot be more than 30 characters",
+          },
+        })}
       />
-      <span className='popup__input-type-error js-input-place-name-input-error'></span>
+      <span className='popup__input-type-error js-input-place-name-input-error'>
+        {errors.name && <p style={{ margin: 0 }}>{errors.name.message}</p>}
+      </span>
       <input
         id='js-input-place-url-input'
         className='popup__form-input js-input-type-place-url'
-        name='link'
-        type='url'
+        type='text'
         placeholder='Url Link'
-        onChange={handleLinkChange}
-        value={link}
-        required
+        {...register("link", {
+          required: "Url is required",
+          pattern: {
+            value: /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+/,
+            message: "Please enter a valid url",
+          },
+        })}
       />
-      <span className='popup__input-type-error js-input-place-url-input-error'></span>
+      <span className='popup__input-type-error js-input-place-url-input-error'>
+        {errors.link && <p style={{ margin: 0 }}>{errors.link.message}</p>}
+      </span>
     </PopupWithForm>
   );
 }
